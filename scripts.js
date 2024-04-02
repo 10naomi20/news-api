5720e57881acf6fa3c3c2bd383973db5efd7277b
10naomi20 <n.uehara1020@gmail.com>
2024-04-03 05:37:09 +0900

    feat: remove unnecessary codes

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const API_KEY = "&apiKey=568e43dbac1e4e08b344e06f40f460a4";
 const TOP_URL = "https://newsapi.org/v2/top-headlines?country=us";
 
 
-async function initialLoad(){
+async function documentLoad(){
   try {
     const response = await fetch(`${TOP_URL}${API_KEY}`);
     const data = await response.json();
@@ -14,7 +14,7 @@ async function initialLoad(){
   }
 }
 
-function clearResults(){
+function clearSearchResults(){
   document.getElementById("option-2-enhanced-results").innerHTML = '';
 }
 function renderToScreen(data){
@@ -47,7 +47,7 @@ function render(data) {
 async function searchbarEventHandler () {
   let input = document.getElementById("searchbar").value;
   input = input.toLowerCase();
-  clearResults();
+  clearSearchResults();
   
 
   let SEARCH_URL = `https://newsapi.org/v2/everything?q=${input}&searchIn=title`;
@@ -63,40 +63,7 @@ async function searchbarEventHandler () {
   }
 }
 
-initialLoad();
+documentLoad();
 
 const searchbar = document.getElementById("searchbar");
 searchbar.addEventListener("keyup", searchbarEventHandler);
-
-
-
-// HOVER JS
-
-// Select the header and card-news elements from the DOM
-let headerElement = document.querySelector('.header');
-let cardNewsElement = document.querySelector('.card-news');
-
-// Function to toggle display
-function toggleDisplay(elementToHide, elementToShow) {
-    // Check if the element to hide is currently displayed
-    if (elementToHide.style.display !== 'none') {
-        elementToHide.style.display = 'none';
-        elementToShow.style.display = 'block';
-    } else {
-        elementToHide.style.display = 'block';
-        elementToShow.style.display = 'none';
-    }
-}
-
- // Initially, you might want to ensure the card-news is not displayed
- cardNewsElement.style.display = 'none';
-
- // Add a 'mouseenter' event listener to the header element
- headerElement.addEventListener('mouseenter', function() {
-     toggleDisplay(headerElement, cardNewsElement);
- });
-
- // Add a 'mouseenter' event listener to the card-news element
- cardNewsElement.addEventListener('mouseenter', function() {
-     toggleDisplay(cardNewsElement, headerElement);
- });
